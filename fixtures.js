c0798c0ded05c2f7b3cdf93a177cc72faebe25cb
suliman abdul <sulimanabdulgadeir@gmail.com>
2018-03-06 14:33:26 +0100

    initialize the project

---
+++ fixtures.js
@@ -0,0 +1,31 @@
+'use strict';
+
+exports.flights = [
+	{
+		reference: 'AF-123',
+		origin: 'CDG',
+		destination: 'BOG',
+		date: new Date(),
+		placesLeft: 30,
+		price: 200,
+		company: 'Air France'
+	},
+	{
+		reference: 'LH-123',
+		origin: 'CDG',
+		destination: 'BOG',
+		date: new Date(),
+		placesLeft: 50,
+		price: 180,
+		company: 'Lufthansa'
+	},
+	{
+		reference: 'AF-323',
+		origin: 'CDG',
+		destination: 'MEX',
+		date: new Date(),
+		placesLeft: 10,
+		price: 280,
+		company: 'Air France'
+	}
+];
---
+++ routes/bookings.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var request = require('request-promise');
+var _ = require('lodash');
+
+var router = express.Router();
+
+router.post('/', async (req, res, next) => {
+	const {body} = req;
+
+	const authentification = req.get('authentification');
+	if(!authentification) {
+		return res.status(401).send({message: 'Il faut envoyer le token'});
+	}
+	
+	if(!body.id_user || !body.id_flight){
+		return res.status(400).send({message: 'Il manque des données'});
+	}
+
+	return res.status(201).send({reference: '123'});
+});
+
+module.exports = router;
