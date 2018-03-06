d73b89ec9af9311f476d32edc336e81e895e85f9
suliman abdul <sulimanabdulgadeir@gmail.com>
2018-03-06 15:34:07 +0100

    Add database

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,22 +1,45 @@
-var express = require('express');
-var request = require('request-promise');
 var _ = require('lodash');
-
+var express = require('express');
+var data = require('../database');
 var router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
 	const {body} = req;
 
 	const authentification = req.get('authentification');
-	if(!authentification) {
+	if (!authentification) {
 		return res.status(401).send({message: 'Il faut envoyer le token'});
 	}
-	
-	if(!body.id_user || !body.id_flight){
-		return res.status(400).send({message: 'Il manque des données'});
+
+	if (_.isEmpty(body)) {
+		return res.status(400).send({message: 'Il manque de données'});
 	}
 
-	return res.status(201).send({reference: '123'});
+	if (!body.id_user || !body.id_flight) {
+		return res.status(400).send({message: 'Il manque de données'});
+	}
+
+	const reference = _.uniqueId('LPDW-VOL-');
+	const {id_user, id_flight} = body;
+	const booking = await data.Bookings.create({
+		reference, id_flight, id_user
+	});
+
+	return res.status(201).send(booking);
+});
+
+router.get('/:reference', async (req, res) => {
+	const authentification = req.get('authentification');
+	if (!authentification) {
+		return res.status(401).send({message: 'Il faut envoyer le token'});
+	}
+
+	const {reference} = req.params;
+	const booking = await data.Bookings.findOne({ where: {reference} });
+  if (!booking) {
+		return res.status(404).send({message: 'La ref n\'existe pas'});
+	}
+	return res.send(booking.dataValues);
 });
 
 module.exports = router;
--- routes/flights.js
+++ routes/flights.js
@@ -3,20 +3,20 @@ var request = require('request-promise');
 var router = express.Router();
 
 router.get('/', async (req, res, next) => {
-	if(req.query && req.query.origin &&
+  if(req.query && req.query.origin &&
      req.query.destination && req.query.date) {
 
     const _flights = await request('http://www.mocky.io/v2/5a9e6e603000007700234c11');
-    const flights = JSON.parse(_flights)
+    const flights = JSON.parse(_flights);
 
     const results = flights.filter(flight => {
       return flight.origin === req.query.origin &&
         flight.destination === req.query.destination;
-    })
+    });
 
-		return res.send(results);
-	}
-	res.status(400).send('Missing data');
+    return res.send(results);
+  }
+  res.status(400).send('Missing data');
 });
 
 module.exports = router;
